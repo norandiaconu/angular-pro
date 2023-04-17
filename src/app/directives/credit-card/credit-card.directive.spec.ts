b9cd468b68e77cca0c00b06c5be51383cd026a96
Noran Diaconu <norandiaconu@gmail.com>
2023-04-17 18:08:58 -0400

    Added tests

--- src/app/directives/credit-card/credit-card.directive.spec.ts
+++ src/app/directives/credit-card/credit-card.directive.spec.ts
@@ -1,8 +1,25 @@
 import { CreditCardDirective } from "./credit-card.directive";
 
 describe("CreditCardDirective", () => {
+  let directive: CreditCardDirective;
+
+  beforeEach(() => {
+    directive = new CreditCardDirective();
+  });
+
   it("should create an instance", () => {
-    const directive = new CreditCardDirective();
     expect(directive).toBeTruthy();
   });
+
+  it("should onKeyDown numbers", () => {
+    let event = { target: { value: "12345678901234567" } } as unknown as KeyboardEvent;
+    directive.onKeyDown(event);
+    expect(directive.border).toBe("");
+  });
+
+  it("should onKeyDown letters", () => {
+    let event = { target: { value: "abc" } } as unknown as KeyboardEvent;
+    directive.onKeyDown(event);
+    expect(directive.border).toBe("5px solid red");
+  });  
 });
